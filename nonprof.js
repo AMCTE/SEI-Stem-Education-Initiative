74696d56b328cefc0ea97495007b20ce2dad379e
AMCTE <127781546+AMCTE@users.noreply.github.com>
2023-09-16 12:02:16 -0500

    Add files via upload

---
+++ nonprof.js
@@ -0,0 +1,35 @@
+//const observer = new IntersectionObserver((entries) => {
+//    entries.forEach((entry) => {
+//        console.log(entry)
+//        if(entry.isIntersecting) {
+//            entry.target.classList.add('show');
+//        } else {
+//            entry.target.classList.remove('show')
+//       }
+ //   });
+//})
+
+//const hiddenElements = document.querySelectorAll('.hidden');
+//hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if(isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+});
+
